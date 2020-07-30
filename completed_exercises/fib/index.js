36084184f1bc532c229b1562dc80e09385033e45
Trejon <trejonstallsworth@gmail.com>
2020-07-30 11:54:46 -0600

    added fib

--- completed_exercises/fib/index.js
+++ completed_exercises/fib/index.js
@@ -36,13 +36,13 @@ module.exports = fib;
 
 // function fib(n) {
 //   const result = [0, 1];
-//
+
 //   for (let i = 2; i <= n; i++) {
 //     const a = result[i - 1];
 //     const b = result[i - 2];
-//
+
 //     result.push(a + b);
 //   }
-//
+
 //   return result[n];
 // }
